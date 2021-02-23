8e9b00426da0d11aac240cf2e3b0dfbb98363bab
RamG007 <48403226+RamG007@users.noreply.github.com>
2021-02-23 18:02:20 +0530

    Update HashCodeQualificationRound.py
    Better but Sooooooooo Slower

--- HashCodeQualificationRound.py
+++ HashCodeQualificationRound.py
@@ -1,4 +1,4 @@
-f = open("C:/Users/admin/Downloads/b_little_bit_of_everything.in", "r")
+f = open("C:/Users/admin/Downloads/a_example", "r")
 
 firstLine = f.readline().split()
 pizzaNumber = int(firstLine[0])
@@ -6,321 +6,154 @@ twoPersonTeams = int(firstLine[1])
 threePersonTeams = int(firstLine[2])
 fourPersonTeams = int(firstLine[3])
 
-deliveredPizzas = 0
 totalTeams = twoPersonTeams + threePersonTeams + fourPersonTeams
 ingredientNumber = -1
+deliveredPizzas = 0
+remainingPizzas = []
+
+def deliverPizzas(persons, teams, pizzas):
+    global ingredientNumber
+    global deliveredPizzas
+
+    temp = []
+    ingredients = []
+
+    while deliveredPizzas < pizzaNumber and len(pizzas) < teams:
+        ingredients_cpy = list(ingredients)
+
+        c = f.readline()
+        if c != EOFError and c != "":
+            match = False
+            ingredientNumber = ingredientNumber + 1
+            ing = c.split()
+            ing.pop(0)
+            for item in ing:
+                if item not in ingredients_cpy:
+                    ingredients_cpy.append(item)
+                else:
+                    match = True                    
+                    for pizza in remainingPizzas:
+                        m = False
+                        ingredients_cpy = list(ingredients)
+                        for pizza_ing in range(1, len(pizza)):
+                            if pizza[pizza_ing] not in ingredients_cpy:
+                                ingredients_cpy.append(pizza[pizza_ing])  
+                            else:
+                                m = True
+                        if m != True:
+                            temp.append(list(pizza))
+                            ingredients = list(ingredients_cpy)
+                            break
+                    break
+            
+            ing.insert(0, ingredientNumber)
+            if match == False:
+                temp.append(list(ing))
+                ingredients = list(ingredients_cpy)
+            else:
+                remainingPizzas.append(list(ing))
+
+            if len(temp) == persons:                
+                for item in temp:
+                    pizzas.append(item)
+                    if item in remainingPizzas:
+                        remainingPizzas.remove(item)
+                temp = []
+                ingredients = []
+                deliveredPizzas = deliveredPizzas + persons
+
+        else:
+            if len(temp) > 0:
+                for item in temp:
+                    for item_ing in range(1, len(item)):
+                        if item[item_ing] in ingredients:
+                            ingredients.remove(item[item_ing])
+                    if item not in remainingPizzas:
+                        remainingPizzas.append(item)                                                
+                    
+            break
+
+
+def checkRemainingPizzas(persons, teams, pizzas):
+    global remainingPizzas
+    
+    temp = []
+    i = 0
+    while (len(remainingPizzas) + len(temp)) >= persons and len(pizzas)/persons < teams:
+        ingredients = []
+        remPizza_cpy = []
+
+        for item in remainingPizzas:
+            remPizza_cpy.append(list(item))
+
+        for item in temp:
+            for item_ing in range(1, len(item)):
+                if item[item_ing] not in ingredients:
+                    ingredients.append(item[item_ing])
+
+        for item in remPizza_cpy:
+            score = 0
+            for item_ing in range(1, len(item)):
+                if item[item_ing] not in ingredients:
+                    score = score + 1
+            item.insert(0, score)
+
+        remPizza_cpy.sort(key= lambda x: x[0])
+        for item in range(len(remPizza_cpy) - 1, -1, -1):
+            ing = list(remPizza_cpy[item])
+            ing.pop(0)
+            if ing not in temp:
+                break
+                      
+        temp.append(ing)      
+
+        if len(temp) == persons:
+            for item in temp:
+                for pizza in remainingPizzas:
+                    if pizza[0] == item[0]:
+                        remainingPizzas.remove(pizza)
+                pizzas.append(item)
+            temp = []
+
+        i = i + 1
+
 
 twoPersonPizzas = []
 threePersonPizzas = []
 fourPersonPizzas = []
 
-twoPersonIngredients = []
-threePersonIngredients = []
-fourPersonIngredients = []
-
-twoTemp = []
-threeTemp = []
-fourTemp = []
+deliverPizzas(2, twoPersonTeams, twoPersonPizzas)
+deliverPizzas(3, threePersonTeams, threePersonPizzas)
+deliverPizzas(4, fourPersonTeams, fourPersonPizzas)
 
 c = f.readline()
-if c != EOFError and c != "":
-    ingredientNumber = ingredientNumber + 1
-    score = 0
-    ing = c.split()
-    ing.pop(0)
-    ing.sort()
-    for item in ing:
-        if item not in twoPersonIngredients:
-            twoPersonIngredients.append(item)
-            score = score + 1
-    ing.insert(0, ingredientNumber)
-    ing.insert(1, 2)
-    ing.insert(2, score)
-    twoTemp.append(list(ing))
-
-c = f.readline()
-if c != EOFError and c != "":
-    ingredientNumber = ingredientNumber + 1
-    score = 0
-    ing = c.split()
-    ing.pop(0)
-    ing.sort()
-    for item in ing:
-        if item not in threePersonIngredients:
-            threePersonIngredients.append(item)
-            score = score + 1
-    ing.insert(0, ingredientNumber)
-    ing.insert(1, 3)
-    ing.insert(2, score)
-    threeTemp.append(list(ing))
-
-c = f.readline()
-if c != EOFError and c != "":
-    ingredientNumber = ingredientNumber + 1
-    score = 0
-    ing = c.split()
-    ing.pop(0)
-    ing.sort()
-    for item in ing:
-        if item not in fourPersonIngredients:
-            fourPersonIngredients.append(item)
-            score = score + 1
-    ing.insert(0, ingredientNumber)
-    ing.insert(1, 4)
-    ing.insert(2, score)
-    fourTemp.append(list(ing))  
-
-while deliveredPizzas < pizzaNumber:
-    c = f.readline()
-    if c != EOFError and c != "":
-        ingredientNumber = ingredientNumber + 1
-        ing = c.split()
-        ing.pop(0)
-        ing.sort()
-        twoScore = 0
-        threeScore = 0
-        fourScore = 0
-        if len(twoPersonPizzas)/2 < twoPersonTeams:
-            #Deliver Two Person Pizzas
-            for item in ing:
-                if item not in twoPersonIngredients:
-                    twoScore = twoScore + 1
-
-        if len(threePersonPizzas)/3 < threePersonTeams:
-            #Deliver Three Person Pizzas
-            for item in ing:
-                if item not in threePersonIngredients:
-                    threeScore = threeScore + 1
-
-        if len(fourPersonPizzas)/4 < fourPersonTeams:
-            #Deliver four Person Pizzas
-            for item in ing:
-                if item not in fourPersonIngredients:
-                    fourScore = fourScore + 1
-
-        
-
-        if len(twoPersonPizzas)/2 < twoPersonTeams and twoScore >= threeScore and twoScore >= fourScore:
-            for item in ing:
-                if item not in twoPersonIngredients:
-                    twoPersonIngredients.append(item)         
-            ing.insert(0, ingredientNumber)
-            ing.insert(1, 2)
-            ing.insert(2, twoScore)
-            twoTemp.append(list(ing))
-
-        elif len(threePersonPizzas)/3 < threePersonTeams and threeScore >= twoScore and threeScore >= fourScore:
-            for item in ing:
-                if item not in threePersonIngredients:
-                    threePersonIngredients.append(item)                    
-            ing.insert(0, ingredientNumber)
-            ing.insert(1, 3)
-            ing.insert(2, threeScore)
-            threeTemp.append(list(ing))
-
-        elif len(fourPersonPizzas)/4 < fourPersonTeams and fourScore >= threeScore and fourScore >= twoScore:
-            for item in ing:
-                if item not in fourPersonIngredients:
-                    fourPersonIngredients.append(item)  
-            ing.insert(0, ingredientNumber)
-            ing.insert(1, 4)
-            ing.insert(2, fourScore)
-            fourTemp.append(list(ing))            
-
-    else:
-        break    
-
-    if len(twoTemp) == 2:
-        for item in twoTemp:
-            twoPersonPizzas.append(item)
-        twoTemp = []
-        deliveredPizzas = deliveredPizzas + 2
-
-    if len(threeTemp) == 3:
-        for item in threeTemp:
-            threePersonPizzas.append(item)
-        threeTemp = []
-        deliveredPizzas = deliveredPizzas + 3
-
-    if len(fourTemp) == 4:
-        for item in fourTemp:
-            fourPersonPizzas.append(item)
-        fourTemp = []
-        deliveredPizzas = deliveredPizzas + 4
-
-    if len(fourPersonPizzas)/4 >= fourPersonTeams and len(threePersonPizzas)/3 >= threePersonTeams and len(twoPersonPizzas)/2 >= twoPersonTeams:
-        break
-
-while len(threePersonPizzas)/3 < threePersonTeams:
-    if len(fourTemp) > 0:
-        tempPizzas = list(fourTemp)
-        for item in tempPizzas:
-            score = 0
-            ingNum = item[0]
-            item.pop(0)
-            item.pop(0)
-            item.pop(0)
-            for ing in item:
-                if ing not in threePersonIngredients:
-                    score = score + 1
-            item.insert(0, ingNum)
-            item.insert(1, score)
-        tempPizzas.sort(key= lambda x: x[1])
-
-        while len(threeTemp) < 3 and len(tempPizzas) > 0:
-            tempPizzas[len(tempPizzas) - 1].insert(1, 3)
-            threeTemp.append(tempPizzas[len(tempPizzas) - 1])
-            for item in fourTemp:
-                if item[0] == tempPizzas[len(tempPizzas) - 1][0]:
-                    fourTemp.remove(item)
-            tempPizzas.pop(len(tempPizzas) - 1)
-        
-        if len(threeTemp) == 3:
-            for item in threeTemp:
-                threePersonPizzas.append(list(item))     
-            threeTemp = []
-            deliveredPizzas = deliveredPizzas + 3          
-    else:
-        break
-
-c = f.readline()
-remainingPizzas = []
 while c != EOFError and c != "":
+    ingredientNumber = ingredientNumber + 1
     ing = c.split()
     ing.pop(0)
-    ingredientNumber = ingredientNumber + 1
-    twoPersonPizzas.sort(key= lambda x: x[2])
-    score = 0
-    for item in ing:
-        if item not in twoPersonIngredients:
-            score = score + 1
-    if twoPersonPizzas[0][2] < score:
-        pizza = twoPersonPizzas[0]
-        twoPersonPizzas.pop(0)
-        for item in ing:
-            if item not in twoPersonIngredients:
-                twoPersonIngredients.append(item)
-        ing.insert(0, ingredientNumber)
-        ing.insert(1, 2)
-        ing.insert(2, score)
-        twoPersonPizzas.append(list(ing))
-        pizza.pop(1)
-        pizza.pop(1)
-        remainingPizzas.append(list(pizza))
-    else:        
-        ing.insert(0, ingredientNumber)
-        remainingPizzas.append(list(ing))
+    ing.insert(0, ingredientNumber)
+    remainingPizzas.append(ing)
     c = f.readline()
 
-count = 0
-while count < len(remainingPizzas):
-    threePersonPizzas.sort(key= lambda x: x[2])
-    ing = list(remainingPizzas[count])
-    ingNum = ing[0]
-    ing.pop(0)
-    score = 0
-    for item in ing:
-        if item not in threePersonIngredients:
-            score = score + 1
-    if threePersonPizzas[0][2] < score:
-        pizza = threePersonPizzas[0]
-        threePersonPizzas.pop(0)
-        for item in ing:
-            if item not in threePersonIngredients:
-                threePersonIngredients.append(item)
-        ing.insert(0, ingNum)
-        ing.insert(1, 3)
-        ing.insert(2, score)
-        threePersonPizzas.append(list(ing))
-        pizza.pop(1)
-        pizza.pop(1)
-        remainingPizzas.insert(count, list(pizza))
-    count = count + 1
+if len(remainingPizzas) > 0:
+    if(len(twoPersonPizzas)/2 < twoPersonTeams):
+        checkRemainingPizzas(2, twoPersonTeams, twoPersonPizzas)
 
-count = 0
-while count < len(remainingPizzas):
-    fourPersonPizzas.sort(key= lambda x: x[2])
-    ing = list(remainingPizzas[count])
-    ingNum = ing[0]
-    ing.pop(0)
-    score = 0
-    for item in ing:
-        if item not in fourPersonIngredients:
-            score = score + 1
-    if fourPersonPizzas[0][2] < score:
-        pizza = fourPersonPizzas[0]
-        fourPersonPizzas.pop(0)
-        for item in ing:
-            if item not in fourPersonIngredients:
-                fourPersonIngredients.append(item)
-        ing.insert(0, ingNum)
-        ing.insert(1, 4)
-        ing.insert(2, score)
-        fourPersonPizzas.append(list(ing))
-        pizza.pop(1)
-        pizza.pop(1)
-        remainingPizzas.insert(count, list(pizza))
-    count = count + 1
+    if(len(threePersonPizzas)/3 < threePersonTeams):
+        checkRemainingPizzas(3, threePersonTeams, threePersonPizzas)
 
+    if(len(fourPersonPizzas)/4 < fourPersonTeams):
+        checkRemainingPizzas(4, fourPersonTeams, fourPersonPizzas)
 
-#count = 0
-#while count < len(remainingPizzas):
-#    twoPersonPizzas.sort(key= lambda x: x[2])
-#    ing = list(remainingPizzas[count])
-#    ingNum = ing[0]
-#    ing.pop(0)
-#    score = 0
-#    for item in ing:
-#        if item not in twoPersonIngredients:
-#            score = score + 1
-#    if twoPersonPizzas[0][2] < score:
-#        pizza = twoPersonPizzas[0]
-#        twoPersonPizzas.pop(0)
-#        for item in ing:
-#            if item not in twoPersonIngredients:
-#                twoPersonIngredients.append(item)
-#        ing.insert(0, ingNum)
-#        ing.insert(1, 2)
-#        ing.insert(2, score)
-#        twoPersonPizzas.append(list(ing))
-#        pizza.pop(1)
-#        pizza.pop(1)
-#        remainingPizzas.insert(count, list(pizza))
-#    count = count + 1
+totalDelivered = len(twoPersonPizzas)//2 + len(threePersonPizzas)//3 + len(fourPersonPizzas)//4
 
-#print(deliveredPizzas)
-teamsDelivered = len(twoPersonPizzas)//2 + len(threePersonPizzas)//3 + len(fourPersonPizzas)//4
-print(teamsDelivered)
-#twoTotalScore = 0
-#threeTotalScore = 0
-#fourTotalScore = 0
+print(totalDelivered)
 
-print("2", end= " ")
-for item in twoPersonPizzas:
-    print(item[0], end= " ")
-    #twoTotalScore = twoTotalScore + item[2]
-print("\n3", end= " ")
-for item in threePersonPizzas:
-    print(item[0], end= " ")
-    #threeTotalScore = threeTotalScore + item[2]
-print("\n4", end= " ")
-for item in fourPersonPizzas:
-    print(item[0], end= " ")
-    #fourTotalScore = fourTotalScore + item[2]
+for i in range(0, len(twoPersonPizzas), 2):
+    print("2", twoPersonPizzas[i][0], twoPersonPizzas[i + 1][0])
 
-#print("\n", twoPersonPizzas)
-#print(threePersonPizzas)
-#print(fourPersonPizzas)
-#print(twoTemp)
-#print(threeTemp)
-#print(fourTemp)
-#print(len(twoPersonPizzas)/2)
-#print(len(threePersonPizzas)/3)
-#print(len(fourPersonPizzas)/4)
-#print(twoTotalScore * twoTotalScore)
-#print(threeTotalScore * threeTotalScore)
-#print(fourTotalScore * fourTotalScore)
-#print(twoPersonIngredients)
-#print(threePersonIngredients)
-#print(fourPersonIngredients)
-#print(remainingPizzas)        
+for i in range(0, len(threePersonPizzas), 3):
+    print("3", threePersonPizzas[i][0], threePersonPizzas[i + 1][0], threePersonPizzas[i + 2][0])
+
+for i in range(0, len(fourPersonPizzas), 4):
+    print("4", fourPersonPizzas[i][0], fourPersonPizzas[i + 1][0], fourPersonPizzas[i + 2][0], fourPersonPizzas[i + 3][0])
